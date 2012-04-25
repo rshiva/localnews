382a755e3f52daed1a9291f611640ca553ba7ac4
Shiva Kumar <nadarshiva.r@gmail.com>
2012-04-25 22:22:41 +0530

    added user display name and moved login to right

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ActiveRecord::Base
     has_secure_password
-    attr_accessible :email,:password,:password_confirmation
+    attr_accessible :email,:password,:password_confirmation,:display_name
     
+    validates_presence_of  :display_name
     validates_uniqueness_of :email ,:display_name
     validates_presence_of :password, :on => :create
     validates_format_of :display_name,:with => /^[-\w\._@]+$/i, :allow_blank => true, :message => "should only contain letters, numbers, or .-_@"
     validates_length_of :password, :minimum => 8, :allow_blank => true
-    #validates_presence_of :email , :display_name
     has_many :posts
     has_many :comments
 end
