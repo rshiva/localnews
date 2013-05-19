48e82cf88b527205e34d96549dc6ed753fffefdd
rshiva <nadarshiva.r@gmail.com>
2013-05-19 16:58:47 +0530

    try to precompile rails validation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Localnews::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+   config.assets.precompile += %w( rails.validations.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
@@ -54,7 +54,6 @@ Localnews::Application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
-  config.assets.precompile += %w( rails.validations.js )
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
